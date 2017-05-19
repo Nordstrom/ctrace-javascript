4160a15fdefa73d3923d32d0d0c3885aa68a37df
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-05-19 11:21:23 -0700

    Initialized tracer with request-promise

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const opentracing = require('opentracing')
+const request = require('request-promise')
 const tracer = require('./lib/tracer.js')
+
+tracer.init(request)
 
 module.exports = Object.assign(tracer, opentracing)
 module.exports.express = require('./lib/middleware/express.js')
 module.exports.request = require('./lib/middleware/request.js')
+
