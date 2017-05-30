31436f3ae4d357c0c9d6aa7b0fe07fcd23c62911
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-05-30 09:37:41 -0700

    Removed extra line

--- index.js
+++ index.js
@@ -6,4 +6,3 @@ const tracer = require('./lib/tracer.js')
 module.exports = Object.assign(tracer, opentracing)
 module.exports.express = require('./lib/middleware/express.js')
 module.exports.request = require('./lib/middleware/request.js')
-
