b51296ca4297c7e98dfe4be85d0bd72de65f35e8
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-07-06 13:45:20 -0700

    Removed .only()

--- test/tracer.spec.js
+++ test/tracer.spec.js
@@ -189,7 +189,7 @@ describe('tracer', () => {
       events.error.should.eql({ foo: 'bar', event: 'ErrorEvent', level: 'error', error: true })
     })
 
-    it.only('can update tracer.debug value on the fly', () => {
+    it('can update tracer.debug value on the fly', () => {
       tracer.init({ debug: false, logFn: function(text) { console.log(text)} })
       let span1 = tracer.startSpan('originating', { debug: true }, () => {})
       tracer.debug({ span: span1 }, 'Debug Log')
